f16b68d8f2a46acdba6af38209922204753bfe9c
Paul Horn <knutwalker@gmail.com>
2012-04-22 14:29:52 +0200

    using old (and working) url scheme

--- fangofmongo/urls.py
+++ fangofmongo/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.defaults import patterns, include
 urlpatterns = patterns('',
     # Example:
     (r'^api/', include('fangofmongo.rest.urls', namespace='api')),
-    (r'^fom/', include('fangofmongo.fom.urls', namespace='fe')),
+    (r'^fangofmongo/', include('fangofmongo.fom.urls', namespace='fe')),
     (r'^$', include('fangofmongo.mangoadmin.urls', namespace='mango')),
 
 )
